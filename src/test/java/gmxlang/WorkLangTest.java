5a8556fcfd078cade51fd08a54e1fd60b9a28977
DudeFromKorea <peteryu24@naver.com>
2024-01-03 09:49:45 +0900

    updated test code

--- src/test/java/gmxlang/WorkLangTest.java
+++ src/test/java/gmxlang/WorkLangTest.java
@@ -6,19 +6,20 @@ import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.*;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.ArgumentMatcher;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 
 import gmx.fwd.mapper.content.ContentMapper;
 import gmx.fwd.service.content.ContentService;
 import gmx.fwd.vo.contentvo.ContentVo;
 
-
+@RunWith(MockitoJUnitRunner.class)
 public class WorkLangTest {
+
 	private ContentService contentService;
 	private ContentMapper mockMapper;
 
@@ -30,25 +31,29 @@ public class WorkLangTest {
 
 	@Test
 	public void addLangWorkTest() {
-		when(mockMapper.checkLangWork(anyString())).thenReturn(true);
+		ContentVo contentVo = new ContentVo("reqName", "resName", "reqLang", "resLang", "note", 1, "00001");
 
-		when(mockMapper.addLangWork(any(ContentVo.class))).thenReturn(1); // mockito
-		int result = contentService.addLangWork("reqName", "resName", "reqLang", "resLang", "note");
+		// Mockito를 사용하여 mockMapper의 동작 정의
+		when(mockMapper.checkLangWork(anyString())).thenReturn(true);
+		when(mockMapper.addLangWork(any(ContentVo.class))).thenReturn(1);
+		int result = contentService.addLangWork(contentVo);
 		assertEquals(1, result);
 	}
 
 	@Test
-	public void addLangWorkDuplicationTest() { // duplication test
+	public void addLangWorkDuplicationTest() {
+		ContentVo contentVo = new ContentVo("reqName", "resName", "reqLang", "resLang", "note", 1, "00001");
 
-		when(mockMapper.addLangWork(any(ContentVo.class))).thenReturn(1);
-		int result = contentService.addLangWork("reqName", "resName", "reqLang", "resLang", "note");
+		// Mockito
+		when(mockMapper.checkLangWork(anyString())).thenReturn(false);
+		int result = contentService.addLangWork(contentVo);
 		assertEquals(-1, result);
 	}
 
 	@Test
 	public void getLangWorkItemSuccessTest() {
 		List<ContentVo> expectedList = new ArrayList<>();
-		expectedList.add(new ContentVo());
+		expectedList.add(new ContentVo(0, null, null, null, null, null, 1, null));
 		when(mockMapper.getLangWorkItem(anyInt())).thenReturn(expectedList); // Mockito
 
 		List<ContentVo> result = contentService.getLangWorkItem(1);
@@ -65,25 +70,20 @@ public class WorkLangTest {
 
 	@Test
 	public void getOrSearchLangWorkTest() {
+		ContentVo contentVo = new ContentVo(null, null, null, null, false);
+
 		List<ContentVo> expectedList = new ArrayList<>();
-		expectedList.add(new ContentVo());
-
-		when(mockMapper.getOrSearchLangWork(argThat(new ArgumentMatcher<HashMap<String, String>>() {
-			@Override
-			public boolean matches(HashMap<String, String> argument) {
-				return argument instanceof HashMap;
-			}
-		}))).thenReturn(expectedList);
-
-		List<ContentVo> result = contentService.getOrSearchLangWork("reqName", "resName", "reqLang", "resLang", "resFlag");
+		expectedList.add(contentVo);
+		when(mockMapper.getOrSearchLangWork(any(ContentVo.class))).thenReturn(expectedList);
+		List<ContentVo> result = contentService.getOrSearchLangWork(contentVo);
 		assertFalse(result.isEmpty());
 	}
 
 	@Test
 	public void modifyLangWorkTest() {
 		when(mockMapper.modifyLangWork(any(ContentVo.class))).thenReturn(true);
-
-		boolean result = contentService.modifyLangWork(1, "resName", "reqName", "reqLang", "resLang", "note");
+		ContentVo contentVo = new ContentVo(0, null, null, null, null, null, 1, null);
+		boolean result = contentService.modifyLangWork(contentVo);
 		assertTrue(result);
 	}
 
@@ -94,5 +94,4 @@ public class WorkLangTest {
 		boolean result = contentService.deleteLangWork(1);
 		assertTrue(result);
 	}
-
 }
