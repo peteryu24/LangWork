d39a50f6cef260878aee1fbfd283f164bea52dd7
DudeFromKorea <peteryu24@naver.com>
2024-01-05 09:52:56 +0900

    changed to restcontroller

--- src/main/java/gmx/fwd/controller/content/ContentController.java
+++ src/main/java/gmx/fwd/controller/content/ContentController.java
@@ -1,26 +1,24 @@
 package gmx.fwd.controller.content;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import gmx.fwd.service.content.ContentService;
 import gmx.fwd.utils.CustomException;
 import gmx.fwd.utils.GmxResult;
 import gmx.fwd.vo.contentvo.ContentVo;
 
-@Controller
+@RestController
 @RequestMapping("/gmxlang")
 public class ContentController {
 
 	@Autowired
 	private ContentService contentService;
 
-	@ResponseBody
 	@PostMapping(value = "/addLangWork")
 	public GmxResult addLangWork(@ModelAttribute ContentVo contentVo) {
 		try {
@@ -30,7 +28,6 @@ public class ContentController {
 		}
 	}
 
-	@ResponseBody
 	@PostMapping(value = "/getLangWorkItem")
 	public GmxResult getLangWorkItem(@RequestParam int mgrSeq) {
 		try {
@@ -40,8 +37,7 @@ public class ContentController {
 		}
 	}
 
-	@ResponseBody
-	@RequestMapping(value = "/getLangWorkList")
+	@PostMapping(value = "/getLangWorkList")
 	public GmxResult getOrSearchLangWork(@ModelAttribute ContentVo contentVo) {
 		try {
 			return contentService.getOrSearchLangWork(contentVo);
@@ -51,7 +47,6 @@ public class ContentController {
 
 	}
 
-	@ResponseBody
 	@PostMapping(value = "/modLangWork")
 	public GmxResult modifyLangWork(@ModelAttribute ContentVo contentVo) {
 		try {
@@ -61,8 +56,7 @@ public class ContentController {
 		}
 	}
 
-	@ResponseBody
-	@RequestMapping(value = "/delLangWork")
+	@PostMapping(value = "/delLangWork")
 	public GmxResult deleteLangWork(@RequestParam int mgrSeq) {
 		try {
 			return contentService.deleteLangWork(mgrSeq);
