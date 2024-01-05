40acd2b549e524187cba1c605c3592f945bd3bfd
DudeFromKorea <peteryu24@naver.com>
2024-01-05 11:01:34 +0900

    remove try -catch

--- src/main/java/gmx/fwd/controller/content/ContentController.java
+++ src/main/java/gmx/fwd/controller/content/ContentController.java
@@ -1,73 +1,46 @@
 package gmx.fwd.controller.content;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import gmx.fwd.service.content.ContentService;
-import gmx.fwd.utils.CustomException;
 import gmx.fwd.utils.GmxResult;
+import gmx.fwd.utils.exception.LangWorkItemNotFoundException;
 import gmx.fwd.vo.contentvo.ContentVo;
 
-@Controller
+@RestController
 @RequestMapping("/gmxlang")
 public class ContentController {
 
 	@Autowired
 	private ContentService contentService;
 
-	@ResponseBody
 	@PostMapping(value = "/addLangWork")
 	public GmxResult addLangWork(@ModelAttribute ContentVo contentVo) {
-		try {
-			return contentService.addLangWork(contentVo);
-		} catch (CustomException e) {
-			return new GmxResult().resultError(e.getMessage());
-		}
+		return contentService.addLangWork(contentVo);
 	}
 
-	@ResponseBody
 	@PostMapping(value = "/getLangWorkItem")
 	public GmxResult getLangWorkItem(@RequestParam int mgrSeq) {
-		try {
-			return contentService.getLangWorkItem(mgrSeq);
-		} catch (CustomException e) {
-			return new GmxResult().resultError(e.getMessage());
-		}
+		return contentService.getLangWorkItem(mgrSeq);
 	}
 
-	@ResponseBody
-	@RequestMapping(value = "/getLangWorkList")
+	@PostMapping(value = "/getLangWorkList")
 	public GmxResult getOrSearchLangWork(@ModelAttribute ContentVo contentVo) {
-		try {
-			return contentService.getOrSearchLangWork(contentVo);
-		} catch (CustomException e) {
-			return new GmxResult().resultError(e.getMessage());
-		}
-
+		return contentService.getOrSearchLangWork(contentVo);
 	}
 
-	@ResponseBody
 	@PostMapping(value = "/modLangWork")
 	public GmxResult modifyLangWork(@ModelAttribute ContentVo contentVo) {
-		try {
-			return contentService.modifyLangWork(contentVo);
-		} catch (CustomException e) {
-			return new GmxResult().resultError(e.getMessage());
-		}
+		return contentService.modifyLangWork(contentVo);
 	}
 
-	@ResponseBody
-	@RequestMapping(value = "/delLangWork")
+	@PostMapping(value = "/delLangWork")
 	public GmxResult deleteLangWork(@RequestParam int mgrSeq) {
-		try {
-			return contentService.deleteLangWork(mgrSeq);
-		} catch (CustomException e) {
-			return new GmxResult().resultError(e.getMessage());
-		}
+		return contentService.deleteLangWork(mgrSeq);
 	}
 }
